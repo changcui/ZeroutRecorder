ba98274e13f094dc025be92bde372d9c36fe2914
changcui <cuichang147@gmail.com>
2017-06-25 04:56:01 +0800

    add EZAudio and launchImage

--- ZeroutRecorder/AppDelegate.swift
+++ ZeroutRecorder/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        Thread.sleep(forTimeInterval: 0.5)
         // Override point for customization after application launch.
         return true
     }
